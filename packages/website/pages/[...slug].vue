918a13ac41f9a0bc042f550b4f8c2cb028d610b2
Harrison Callahan <harrison@harrisoncallahan.com>
2023-09-21 11:40:51 -0700

    [C] Tidies up the 404 page.

--- packages/website/pages/[...slug].vue
+++ packages/website/pages/[...slug].vue
@@ -1,11 +1,12 @@
+<!-- TODO: could this be cooler? Maybe add a game here! -->
 <template>
-  <NuxtLayout>
-    <div class="flex h-screen flex-col items-center justify-center">
-      <h1 class="text-6xl font-bold">
+  <NuxtLayout class="bg-primary-lightest">
+    <div class="container flex h-full flex-col items-center justify-center gap-8 text-center">
+      <h1 class="font-heading text-6xl">
         404 - That page can't be found... :-[
       </h1>
-      <p class="text-2xl">
-        I'm really not sure what happened here. Maybe check back in a little bit to see if we fixed it.
+      <p class="font-heading text-2xl">
+        I'm really not sure what happened here but this is kind of embarassing... Maybe check back in a little bit to see if we fixed it. 🤷
       </p>
     </div>
   </NuxtLayout>
