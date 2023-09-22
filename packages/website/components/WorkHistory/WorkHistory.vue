4f27894fe6904c0cd75d6f9ff369bfd8dd8e22e8
Harrison Callahan <harrison@harrisoncallahan.com>
2023-09-22 10:04:27 -0700

    [F] Adds final touches to WorkHistory.

--- packages/website/components/WorkHistory/WorkHistory.vue
+++ packages/website/components/WorkHistory/WorkHistory.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="flex w-full flex-col gap-3">
     <template v-for="(company, index) in companies" :key="company">
-      <div class="flex h-52 flex-col items-center justify-center rounded-2xl border-2 border-solid border-black bg-red-400 font-heading transition-opacity">
-        <p class="text-lg font-bold">
+      <div class="flex aspect-video w-full flex-col items-center justify-center rounded-2xl border-2 border-solid border-black bg-secondary font-heading transition-opacity">
+        <p class="text-2xl font-bold text-black">
           {{ company.attributes.name }}
         </p>
-        <p v-for="(job, i) in company.attributes.jobs.data" :key="i" class="text-sm">
+        <p class="mt-4 text-lg text-black">
+          Duration:
+        </p>
+        <p class="text-sm text-gray-800">
+          {{ getFormattedDate(company.attributes.date_from) }} - {{ getFormattedDate(company.attributes.date_to) }}
+        </p>
+        <p class="mt-4 text-lg text-black">
+          Roles:
+        </p>
+        <p v-for="(job, i) in company.attributes.jobs.data" :key="i" class="text-sm text-gray-800">
           {{ job.attributes.name }}
         </p>
-        <p class="text-sm" />
       </div>
       <WorkHistoryConnector v-if="companies.length > index + 1" />
     </template>
@@ -24,4 +32,14 @@ const result = await useStrapi<ApiCompanyCompany['attributes']>().find(collectio
   sort: 'order:asc',
 })
 const companies = result.data
+
+const getFormattedDate = (date: string) => {
+  if (!date) { return 'Present' }
+
+  const dateObj = new Date(date)
+  return dateObj.toLocaleDateString('en-US', {
+    year: 'numeric',
+    month: 'long',
+  })
+}
 </script>
