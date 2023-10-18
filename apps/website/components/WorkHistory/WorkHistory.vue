df68a4d7e080228a170bedeb1fd6c469ebd5f0c2
Harrison Callahan <harrison@harrisoncallahan.com>
2023-10-18 13:33:33 -0700

    [C] Cleans up WorkHistory styles.

--- apps/website/components/WorkHistory/WorkHistory.vue
+++ apps/website/components/WorkHistory/WorkHistory.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="flex w-full flex-col gap-3">
     <template v-for="(company, index) in companies" :key="company">
-      <div class="flex aspect-video w-full flex-col items-center justify-center rounded-2xl border-2 border-solid border-black bg-secondary font-heading transition-opacity">
-        <p class="text-2xl font-bold text-black">
+      <div class="flex w-full flex-col items-center justify-center rounded-2xl border-2 border-solid border-black bg-secondary p-8 font-heading transition-opacity sm:aspect-video">
+        <p class="text-base font-bold text-black sm:text-2xl">
           {{ company.attributes.name }}
         </p>
-        <p class="mt-4 text-lg text-black">
+        <p class="mt-4 text-center text-sm text-black sm:text-lg">
           Duration:
         </p>
-        <p class="text-sm text-gray-800">
+        <p class="text-center text-xs text-gray-800 sm:text-sm">
           {{ getFormattedDate(company.attributes.date_from) }} - {{ getFormattedDate(company.attributes.date_to) }}
         </p>
-        <p class="mt-4 text-lg text-black">
-          Roles:
+        <p class="mt-4 text-center text-sm text-black sm:text-lg">
+          {{ company.attributes.jobs.data.length > 1 ? 'Roles:' : 'Role:' }}
         </p>
-        <p v-for="(job, i) in company.attributes.jobs.data" :key="i" class="text-sm text-gray-800">
+        <p v-for="(job, i) in company.attributes.jobs.data" :key="i" class="text-center text-xs text-gray-800 sm:text-sm">
           {{ job.attributes.name }}
         </p>
       </div>
