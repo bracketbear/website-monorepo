6987bc6b3d89932b9f79862f083fee2a5f22abd5
Harrison Callahan <harrison@harrisoncallahan.com>
2024-12-22 11:51:45 -0800

    [C] Fixes linting.

--- apps/next-ui/eslint.config.mjs
+++ apps/next-ui/eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import tailwind from "eslint-plugin-tailwindcss";
+import eslintConfigPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -11,6 +13,8 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...tailwind.configs["flat/recommended"],
+  eslintConfigPrettierRecommended,
 ];
 
 export default eslintConfig;
--- apps/next-ui/src/app/page.tsx
+++ apps/next-ui/src/app/page.tsx
@@ -2,19 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   const thisYear = new Date().getFullYear();
-  
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <header className="row-start-1 flex gap-6 items-center justify-center">
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
+      <header className="row-start-1 flex items-center justify-center gap-6">
         <Image src="/logo.svg" alt="Logo" width={48} height={48} />
         <h1 className="text-2xl font-bold">Bracket Bear</h1>
       </header>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <div>
-          Home Page
-        </div>
+      <main className=" row-start-2 flex flex-col  items-center gap-8 sm:items-start ">
+        <div>Home Page</div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center bg-red-950">
+      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-6 bg-red-950">
         Copyright &copy;2022-{thisYear} Harrison Callahan. All rights reserved.
       </footer>
     </div>
