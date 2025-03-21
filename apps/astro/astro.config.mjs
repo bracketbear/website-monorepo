accacb3d9727960653ae1e113746f098b6285dc1
Harrison Callahan <harrison@harrisoncallahan.com>
2025-03-21 16:11:38 -0700

    Fixes static building.

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -3,18 +3,23 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import keystatic from '@keystatic/astro';
-
 import markdoc from '@astrojs/markdoc';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), markdoc(), keystatic()],
+  integrations: [
+    react(),
+    markdoc(), 
+    // Bug when trying to statically render website. This disabless Keystatic when building.
+    !import.meta.env.PROD ? keystatic() : null
+  ],
   vite: {
     plugins: [
       // @ts-expect-error: Bug with TailwindCSS Vite plugin type definition
       tailwindcss(),
     ],
   },
+  output: 'static',
   experimental: {
     // Enable experimental support for SVG images as components
     svg: true,
