69b7702f79e7da8ad6ced36b3e924a9ba96ad826
Harrison Callahan <harrison@harrisoncallahan.com>
2025-04-03 11:19:13 -0700

    Trying to fix eslint issues conflicting with Prettier. Basically made Eslint the source of truth. Also fixes magic number madness.

--- apps/astro/eslint.config.js
+++ apps/astro/eslint.config.js
@@ -1,41 +1,70 @@
-import eslintPluginAstro from "eslint-plugin-astro";
-import love from "eslint-config-love";
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import eslintPluginAstro from 'eslint-plugin-astro';
+import { defineConfig } from 'eslint/config';
+import love from 'eslint-config-love';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
-export default [
+export default defineConfig([
   love,
   ...eslintPluginAstro.configs.recommended,
   eslintPluginPrettierRecommended,
   {
     rules: {
-      "comma-dangle": "off",
+      'comma-dangle': 'off',
+      '@typescript-eslint/no-magic-numbers': [
+        'error',
+        {
+          detectObjects: false,
+          ignoreArrayIndexes: true,
+          ignoreDefaultValues: true,
+          ignore: [-1, 0, 1, 2],
+        },
+      ],
+      'prettier/prettier': [
+        'error',
+        {
+          trailingComma: 'es5',
+          tabWidth: 2,
+          semi: true,
+          singleQuote: true,
+          printWidth: 80,
+          endOfLine: 'lf',
+          bracketSpacing: true,
+        },
+      ],
     },
     ignores: [
-      "**/node_modules/**",
-      "dist/",
-      ".astro/",
-      ".env",
-      ".env.production",
-      "npm-debug.log*",
-      "yarn-debug.log*",
-      "yarn-error.log*",
-      "pnpm-debug.log*",
-      ".vscode/",
-      ".idea/",
-      "src/assets/",
+      '**/node_modules/**',
+      'dist/',
+      '.astro/',
+      '.env',
+      '.env.production',
+      'npm-debug.log*',
+      'yarn-debug.log*',
+      'yarn-error.log*',
+      'pnpm-debug.log*',
+      '.vscode/',
+      '.idea/',
+      'src/assets/',
     ],
   },
   {
-    files: ["*.astro"],
-    parser: "astro-eslint-parser",
+    files: ['*.astro'],
+    parser: 'astro-eslint-parser',
     parserOptions: {
-      parser: "@typescript-eslint/parser",
-      extraFileExtensions: [".astro"],
-      project: "./tsconfig.json",
+      parser: '@typescript-eslint/parser',
+      extraFileExtensions: ['.astro'],
+      project: './tsconfig.json',
     },
-    extends: ["plugin:astro/recommended"],
+    extends: [...eslintPluginAstro.configs.recommended],
     rules: {
-      quotes: ["error", "single"],
+      quotes: ['error', 'single'],
+      'comma-dangle': 'off',
+      'prettier/prettier': [
+        'error',
+        {
+          parser: 'astro',
+        },
+      ],
     },
   },
-];
+]);
