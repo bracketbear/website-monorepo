1c617d7ed8febe9e5582c087f86f6d70ac14ea42
Harrison Callahan <harrison@harrisoncallahan.com>
2025-06-27 09:42:03 -0700

    Deletes old website.

--- apps/website/layouts/transparent-nav.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="relative">
-    <AppNavigation :transparent="true" class="!absolute z-10 w-full" @mousemove.stop />
-    <div class="grid-cols-[1,-1] grid-rows-[1,2]">
-      <slot />
-    </div>
-    <Footer class="grid-cols-[1,-1] grid-rows-[-1,-2]" />
-  </div>
-</template>
--- apps/website/plugins/click-outside.ts
+++
@@ -1,6 +0,0 @@
-import { defineNuxtPlugin } from '#app'
-import ClickOutsideDirective from '~/directives/click-outside'
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.directive('click-outside', ClickOutsideDirective)
-})
